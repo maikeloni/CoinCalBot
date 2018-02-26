c044dff209955ee43e52cd5bd75b95454d165db1
Michael Füllbier <michael.fuellbier@posteo.de>
2018-02-26 20:29:22 +0100

    Check for config.txt added

--- launcher.py
+++ launcher.py
@@ -13,7 +13,10 @@ categories_med = config.get('Discord', 'categories_med').split(",")
 days_short = config.get('Discord', 'days_short')
 categories_short = config.get('Discord', 'categories_short').split(",")
 
-while True:
-    bot.discord(url, days_long, categories_long, days_med, categories_med, days_short, categories_short)
-    print("Sleep for 1h before next fetching...")
-    time.sleep(3600)
+if days_short >= days_med or days_med >= days_long:
+    print("Error: You have to set days_short < days_med < days_long")
+else:
+    while True:
+        bot.discord(url, days_long, categories_long, days_med, categories_med, days_short, categories_short)
+        print("Sleep for 1h before next fetching...")
+        time.sleep(3600)
